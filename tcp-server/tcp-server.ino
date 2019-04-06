c1f92fbc3d42306eba8cb4c40c0957b655d6976a
Andrew Pashinnik <tobirawork@gmail.com>
2019-04-06 22:38:18 +0300

    feat: send data from tcp-server to arduino

--- tcp-server/tcp-server.ino
+++ tcp-server/tcp-server.ino
@@ -1,56 +1,69 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
+#include <WiFiClient.h>
 #include <SoftwareSerial.h>
 
 const char *ssid = "JOPA";
 const char *password = "qjuehn123";
 
-WiFiServer server(23); //   192.168.4.1
+const int rxPin = D2;
+const int txPin = D3;
 
-SoftwareSerial s(D6, D5);
+SoftwareSerial NodeMCU(rxPin, txPin);
 
-void setup() {
+WiFiServer server(23); // 192.168.4.1
+
+void setup()
+{
   delay(1000);
+
+  NodeMCU.begin(115200);
   Serial.begin(9600);
-  s.begin(9600);
+
+  pinMode(D2, INPUT);
+  pinMode(D3, OUTPUT);
 
   WiFi.softAP(ssid, password);
-
   IPAddress myIP = WiFi.softAPIP();
 
-  Serial.print("Start TCP server on ");
+  Serial.print("Start TCP(telnet) server on ");
   Serial.println(myIP.toString());
 
-  server.begin(); // start telnet server
+  server.begin();
   server.setNoDelay(true);
 }
 
-void loop() {
-  if(server.hasClient()){
+void loop()
+{
+  if (server.hasClient())
+  {
     digitalWrite(LED_BUILTIN, LOW);
 
     WiFiClient client = server.available();
 
-    if (client.connected()) {
-      Serial.println("Con client");
-    
-      while(client.connected()) {
+    if (client.connected())
+    {
+      Serial.println("Connected to client!");
+
+      String msg = "";
+      while (client.connected())
+      {
         int ch = client.read();
-        if (ch != -1) {
+        if (ch != -1)
+        {
           char c = ch;
-          Serial.print(c);
-          s.write(ch);
-//          Serial.write(ch);
-          
+          msg.concat(c);
         }
-        
-        // Serial.write();
       }
-  
-      Serial.println(" Discon client");
+
+      Serial.print(msg);
+      NodeMCU.print(msg);
+
+      Serial.println("\nDisconnected from client!");
       client.stop();
     }
-  } else{
+  }
+  else
+  {
     digitalWrite(LED_BUILTIN, HIGH);
   }
-} 
+}
