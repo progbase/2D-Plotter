9aa57065a4a3277c56e356ec1588152bfcbf253d
Andrew Pashinnik <tobirawork@gmail.com>
2019-04-07 11:00:55 +0300

    refactor(uno-serial-reciever)

--- uno-serial-reciever/uno-serial-reciever.ino
+++ uno-serial-reciever/uno-serial-reciever.ino
@@ -5,22 +5,19 @@ const int txPin = 1;
 
 SoftwareSerial ArduinoUNO(rxPin, txPin);
 
-void setup()
-{
-  ArduinoUNO.begin(115200);
-  Serial.begin(9600);
+void setup() {
+    ArduinoUNO.begin(115200);
+    Serial.begin(9600);
 
-  Serial.println("Started reading from NODE MCU!");
-  pinMode(LED_BUILTIN, OUTPUT);
+    Serial.println("Started reading from NODE MCU!");
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop()
-{
-  if (ArduinoUNO.available())
-  {
-    Serial.print("Message: ");
-    Serial.print(ArduinoUNO.readString());
-    Serial.println("\n----------------------");
-  }
-  delay(300);
+void loop() {
+    if (ArduinoUNO.available()) {
+        Serial.print("Message: ");
+        Serial.print(ArduinoUNO.readString());
+        Serial.println("\n----------------------");
+    }
+    delay(300);
 }
